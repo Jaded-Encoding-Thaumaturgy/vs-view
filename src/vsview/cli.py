0df75fa27c694b01ea76f55f2ea2707c9d7d3497
Vardë <ichunjo.le.terrible@gmail.com>
2026-06-11 19:57:57 +0200

    cli: use "true" instead of "1" for boolean env vars

--- src/vsview/cli.py
+++ src/vsview/cli.py
@@ -83,18 +83,18 @@ def main(argv: Sequence[str] | None = None) -> None:
         raise SystemExit(0)
 
     # Setup env vars
-    os.environ["JETPYTOOLS_NO_COLOR"] = "1"
+    os.environ["JETPYTOOLS_NO_COLOR"] = "true"
     os.environ["PYDANTIC_ERRORS_INCLUDE_URL"] = "false"
     if cfg.hdr:
         os.environ.setdefault("QSG_RHI_HDR", "scrgb")
         os.environ.setdefault("QSG_INFO", "1")
 
     if cfg.settings_roaming:
-        os.environ["VSVIEW_GLOBAL_SETTINGS_ROAMING"] = "1"
+        os.environ["VSVIEW_GLOBAL_SETTINGS_ROAMING"] = "true"
     if cfg.settings_env:
-        os.environ["VSVIEW_GLOBAL_SETTINGS_ENVIRONMENT"] = "1"
+        os.environ["VSVIEW_GLOBAL_SETTINGS_ENVIRONMENT"] = "true"
     if cfg.settings_env_copy:
-        os.environ["VSVIEW_GLOBAL_SETTINGS_ENVIRONMENT_COPY"] = "1"
+        os.environ["VSVIEW_GLOBAL_SETTINGS_ENVIRONMENT_COPY"] = "true"
     if cfg.hdr:
         os.environ["VSVIEW_HDR"] = "true"
 
