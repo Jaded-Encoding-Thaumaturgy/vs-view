3665921f541fc02f4c0386b443e89e8ea959eff2
Vardë <ichunjo.le.terrible@gmail.com>
2026-08-06 01:24:13 +0200

    editor: suppress chromium stderr logs

--- src/workspaces/editor/src/python/vsview_editor/__init__.py
+++ src/workspaces/editor/src/python/vsview_editor/__init__.py
@@ -1,9 +1,19 @@
-from vsview.api import hookimpl
-from vsview.app.workspace import BaseWorkspace
+import logging
+import os
+from typing import Any
+
+from vsview.api import get_console_level, hookimpl
+from vsview.api.workspace import PluginBaseWorkspace
+
+# Suppress internal Chromium C++ stderr output (LOG_FATAL = 3)
+if get_console_level() > logging.DEBUG:
+    name = "QTWEBENGINE_CHROMIUM_FLAGS"
+    os.environ[name] = " ".join([os.environ.setdefault(name, ""), "--log-level=3"]).strip()
+
 
 from .workspace import EditorWorkspace
 
 
 @hookimpl
-def vsview_register_workspace() -> type[BaseWorkspace]:
+def vsview_register_workspace() -> type[PluginBaseWorkspace[Any, Any]]:
     return EditorWorkspace
